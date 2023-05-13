bd6c7e71e0cc35317ed00772ef784d9e04a842f3
Raju <apple@Rajud-MacBook-Pro.local>
2023-05-13 12:21:05 +0530

    added regular structure

--- review/src/main/java/com/product/review/controller/ReviewController.java
+++ review/src/main/java/com/product/review/controller/ReviewController.java
@@ -1,6 +1,13 @@
 package com.product.review.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +20,28 @@ public class ReviewController {
 
 	@Autowired
 	private ReviewService reviewService;
+
+	@GetMapping
+	public Map<String, Object> getReview(@RequestBody Map<String, Object> review) {
+		return null;
+
+	}
+
+	@PutMapping
+	public Map<String, Object> updateReview(@RequestBody Map<String, Object> review) {
+		return null;
+
+	}
+
+	@DeleteMapping
+	public Map<String, Object> deleteReview(@RequestBody Map<String, Object> review) {
+		return null;
+
+	}
+
+	@PostMapping
+	public Map<String, Object> addReview(@RequestBody Map<String, Object> review) {
+		return null;
+
+	}
 }
--- review/src/main/java/com/product/review/entities/Review.java
+++ review/src/main/java/com/product/review/entities/Review.java
@@ -1,12 +1,18 @@
 package com.product.review.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@Table
 @Entity
 @Data
 @AllArgsConstructor
 public class Review {
 
+	Integer productId;
+	Float averageReviewScore;
+	Integer numberOfReviews;
+	
 }
