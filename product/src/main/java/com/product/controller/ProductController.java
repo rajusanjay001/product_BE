1725815e9d9c01c370ce896fcc888c7d77fffed4
Raju <apple@Rajud-MacBook-Pro.local>
2023-05-15 00:22:33 +0530

    adding review services api call to product service

--- product/src/main/java/com/product/controller/ProductController.java
+++ product/src/main/java/com/product/controller/ProductController.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +19,9 @@ public class ProductController {
 	@Autowired
 	private ProductService productSetrvice;
 
-	@GetMapping
-	public Map<String, Object> getProductDetails() {
-		productSetrvice.getProduct("BB5476");
-		return new HashMap<>();
+	@GetMapping("/{product_id}")
+	public ResponseEntity<Map<String, Object>> getProductDetails(@PathVariable  String product_id) {
+		productSetrvice.getProduct(product_id);
+		return null;
 	}
 }
--- product/src/main/java/com/product/service/impl/ProductServiceImpl.java
+++ product/src/main/java/com/product/service/impl/ProductServiceImpl.java
@@ -7,8 +7,10 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -20,39 +22,53 @@ import com.product.service.ProductService;
 @Service
 public class ProductServiceImpl implements ProductService {
 
-//	@Value("${product.detail.url}")
-//	protected String productDetailUrl;
-@Autowired
-private RestTemplate restTemplate;
+	@Value("${product.detail.url}")
+	protected String productDetailUrl;
+
+	@Value("${product.review.url}")
+	protected String reviewUrl;
+
+	@Autowired
+	private RestTemplate restTemplate;
 	private Logger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
 
 	@Override
 	public Map<String, Object> getProduct(String productId) {
-	String productDetailUrl = "https://www.adidas.co.uk/api/products";
-//	ResponseEntity<Object> response;
-	try {	
-	URI uri = new URI(productDetailUrl+"/"+productId);
-//		response
-//		  = restTemplate.exchange(uri, HttpMethod.GET, null,Object.class);
-//		response.getBody();
-//		response.getStatusCode();
-	HttpHeaders headers = new HttpHeaders(); 
-	headers.setContentType(MediaType.APPLICATION_JSON); 
-	RequestEntity requestEntity = new RequestEntity("", headers, HttpMethod.GET, 
-			URI.create("https://api.publicapis.org/entries")); 
-	
-	ResponseEntity<String> responseEntity = 
-	restTemplate.exchange(requestEntity, String.class); 
-	responseEntity.getBody();
-//		(URI url, HttpMethod method, @Nullable HttpEntity<?> requestEntity,
-//				Class<T> responseType)
-	}catch(Exception e) {
-		
-	}
-		
-//		Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
-		
+		getReview(productId);
+//		String productDetailUrl = "https://www.adidas.co.uk/api/products";
+//		try {
+//			URI uri = new URI(productDetailUrl + "/" + productId);
+//
+//			HttpHeaders headers = new HttpHeaders();
+//			headers.setContentType(MediaType.APPLICATION_JSON);
+//			RequestEntity requestEntity = new RequestEntity("", headers, HttpMethod.GET,
+//					URI.create("https://api.publicapis.org/entries"));
+//
+//			ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, String.class);
+//			responseEntity.getBody();
+//		} catch (Exception e) {
+//
+//		}
+
 		return new HashMap<>();
+	}
+
+	public Map<String, Object> getReview(String productId) {
+		Map<String, Object> body = new HashMap<>();
+		try {
+			URI uri = new URI(reviewUrl + "/" + productId);
+
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			RequestEntity requestEntity = new RequestEntity("", headers, HttpMethod.GET, uri);
+
+			ResponseEntity<Map> responseEntity = restTemplate.exchange(requestEntity, Map.class);
+			body = responseEntity.getBody();
+		} catch (Exception e) {
+			logger.error("Exception in getting review for the product", e);
+		}
+
+		return body;
 	}
 
 }
